09005bbe316a10b560ab0fece97b1c8c187c087b
chu4zu9 <uesugirennn@yahoo.co.jp>
2021-07-12 06:40:35 +0900

    add:グローバルな状態管理を行うためのStateProviderを追加. グローバルにboolean値を管理する.

---
+++ context/StateProvider.tsx
@@ -0,0 +1,19 @@
+import React, { createContext, useState, useContext } from 'react'
+
+const StateContext = createContext(
+  {} as {
+    toggle: boolean
+    setToggle: React.Dispatch<React.SetStateAction<boolean>>
+  }
+)
+
+export const StateProvider: React.FC = ({ children }) => {
+  const [toggle, setToggle] = useState(false)
+  return (
+    <StateContext.Provider value={{ toggle, setToggle }}>
+      {children}
+    </StateContext.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(StateContext)
